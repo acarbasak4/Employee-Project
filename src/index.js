ab067647d046ec19e2f7e4b3bfb35085d688f60d
Başak Acar <acarbasak4@gmail.com>
2020-12-24 21:39:56 +0300

    added request process

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-console.log("Test");
-console.log("Test2");
-console.log("Test3"); 
+import {Request} from "./requests";
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update"); 
+
+const request = new Request("http://localhost:3000/employees");
+
+// request.get()
+// .then(employees => console.log(employees))
+// .catch(err=> console.log(err));
+
+
+// request.post({name:"Oğuzhan Arslan", department:"Game Developer", salary:4500 })
+// .then(employee => console.log(employee))
+// .catch(err => console.log(err));
+
+// request.put(3,{name:"Oğuzhan Arslan", department: "FrontEnd Developer", salary: 5000 } )
+// .then(employee=> console.log(employee))
+// .catch(err=> console.log(err));
+
+request.delete(3)
+.then(message=>console.log(message) )
+.catch(err=> console.log(err));
